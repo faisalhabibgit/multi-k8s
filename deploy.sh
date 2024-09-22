fd4d504675138ba15f7d6d4ab9ca861898d83af9
Faisal <faisal@Faisals-MacBook-Pro.local>
2024-09-22 00:36:47 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t faiscodingdocker/multi-client:latest -t faiscodingdocker/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t faiscodingdocker/multi-server:latest -t faiscodingdocker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t faiscodingdocker/multi-worker:latest -t faiscodingdocker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push faiscodingdocker/multi-client:latest
+docker push faiscodingdocker/multi-server:latest
+docker push faiscodingdocker/multiworker:latest
+docker push faiscodingdocker/multi-client:$SHA
+docker push faiscodingdocker/multi-server:$SHA
+docker push faiscodingdocker/multiworker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=faiscodingdocker/multi-server:$SHA
+kubectl set image deployments/client-deployment client=faiscodingdocker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=faiscodingdocker/multi-worker:$SHA
